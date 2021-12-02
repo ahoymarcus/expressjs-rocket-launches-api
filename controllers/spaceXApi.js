21ef974ca2c46f79a6dca9b6cddec7301ae874b4
Marcus Vinícius da Silveira richa <marcus.richa@sempreceub.com>
2021-12-01 22:12:57 -0300

    Adicionar urls de dados externos.

--- controllers/spaceXApi.js
+++ controllers/spaceXApi.js
@@ -1,19 +1,20 @@
 // CONTROLLERS
-// https://docs.microsoft.com/en-us/connectors/rspacexip/#get-past-launches
-// https://docs.microsoft.com/en-us/connectors/rspacexip/#get-latest-launch
-// https://docs.microsoft.com/en-us/connectors/rspacexip/#get-next-launch
-// https://docs.microsoft.com/en-us/connectors/rspacexip/#get-all-launches
-// https://github.com/r-spacex/SpaceX-API/blob/master/routes/launches/v4/index.js
-// https://codehandbook.org/how-to-make-rest-api-calls-in-express-web-app/
-//https://stackoverflow.com/questions/44903053/how-to-call-an-api-from-another-api-in-expressjs
-
 
 
 // require API_helper.js
 const api_helper = require('../API_helper');
 
 
-const url = "https://api.spacexdata.com/v4/launches/";
+// Get All Launches
+//const url = "https://api.spacexdata.com/v4/launches/";
+// Get Next Launch
+const nextLaunchUrl = "https://api.spacexdata.com/v4/launches/next";
+// Get Next Launches
+const nextLaunchesUrl = "https://api.spacexdata.com/v4/launches/upcoming";
+// Get Last Launch
+const lastLaunchUrl = "https://api.spacexdata.com/v4/launches/latest";
+// Get Past Launches
+const pastLaunchesUrl = "https://api.spacexdata.com/v4/launches/past";
 
 
 
